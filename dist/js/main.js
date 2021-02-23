97ff81e64bbcd83dd06ad894557bf22117aa4963
Sveta <paranyushkina.sv@gmail.com>
2021-02-23 16:19:11 +0300

    header

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.icon').click(function() {
+    $('.icon').on('click',function() {
         $('.icon').toggleClass('active');
         $('.header__nav').toggleClass('is-open');
     });
